7fc44f0bfd50d25f2b7cbe73c710ad185e55c804
Evgeniy Work <zheka0072007@rambler.ru>
2021-10-18 12:27:24 +0300

    Скрытие Табов

---
+++ js/script.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', () =>{
+
+    // Задача 1. Табы
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParrent = document.querySelector('.tabheader__items');
+
+    function hideTabContent () {
+        tabsContent.forEach( item => {
+            item.style.display = 'none';
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+     });  
+    }
+
+    function showTabContent(i=0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+          
+});
